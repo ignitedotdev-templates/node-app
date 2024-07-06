e002cb67ebe9cec627942483ab286c9fe062637f
Precious Okwu <32944039+libracoder@users.noreply.github.com>
2024-07-06 01:54:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get("/books", (req, res) => {
 
 // Start the server
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}");
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
